74894e75dd76e5732f83571caeaa678782aca76b
Kevin Steptoe <kevin.steptoe@gmail.com>
2022-01-09 14:47:38 +0000

    Initial working extension

--- src/pyscaffoldext/clickstart/extension.py
+++ src/pyscaffoldext/clickstart/extension.py
@@ -1,35 +1,47 @@
 from typing import List
+from pathlib import Path
 
-from pyscaffold.actions import Action  # ActionParams, ScaffoldOpts, Structure
+from pyscaffold.actions import Action, ActionParams, ScaffoldOpts, Structure
 from pyscaffold.extensions import Extension
 
-# from pyscaffold.operations import no_overwrite
-# from pyscaffold.structure import merge
-# from pyscaffold.templates import get_template
+from pyscaffold.operations import no_overwrite
+from pyscaffold.structure import create, merge, reject
+from pyscaffold.templates import get_template
+from . import templates as my_templates
 
 
 class Clickstart(Extension):
     """
-    This class serves as the skeleton for your new PyScaffold Extension. Refer
-    to the official documentation to discover how to implement a PyScaffold
-    extension - https://pyscaffold.org/en/latest/extensions.html
-    """
+        This class inherits Extension and creates an extension that uses click as the CLI
+        """
 
     def activate(self, actions: List[Action]) -> List[Action]:
-        """Activate extension. See :obj:`pyscaffold.extension.Extension.activate`."""
-        # actions = self.register(actions, add_files)
+        """Activates See :obj:`pyscaffold.extension.Extension.activate`."""
+
+        actions = self.register(actions, add_file)
+        # ^ First an add extension.
+
+        actions = self.register(actions, reject_file)
+        # ^ Activate a reject to prevent default skeleton being generated.
+
         return actions
 
 
-# def add_files(struct: Structure, opts: ScaffoldOpts) -> ActionParams:
-#     """Add custom extension files. See :obj:`pyscaffold.actions.Action`"""
+def add_file(struct: Structure, opts: ScaffoldOpts) -> ActionParams:
+    """Adds the click_skeleton template. See :obj:`pyscaffold.actions.Action`"""
 
-#     template = get_template("awesome_file", relative_to=__name__)
-#     test_template = get_template("test_awesome_file", relative_to=__name__)
+    template = get_template("click_skeleton", relative_to=my_templates)
 
-#     files: Structure = {
-#         "src": {opts["package"]: {"awesome_file.py": (template, no_overwrite())}},
-#         "tests": {"test_awesome_file.py": (test_template, no_overwrite())},
-#     }
+    files: Structure = {
+        "src": {opts["package"]: {"click_skeleton.py": (template, no_overwrite(create))}}
+    }
 
-#     return merge(struct, files), opts
+    return merge(struct, files), opts
+
+
+def reject_file(struct: Structure, opts: ScaffoldOpts) -> ActionParams:
+    """Rejects the default skeleton template. See :obj:`pyscaffold.actions.Action`"""
+
+    file = Path("src", opts["package"], "skeleton.py")
+
+    return reject(struct, file), opts
